d768fd4cfa71cc9dae2cd9cd0afb1acde325abba
seungrodotlee <srlee0226w@gmail.com>
2022-03-29 16:57:28 +0900

    [22.03.29:16:57] Init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/resume/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js%/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 8888,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public/static",
+          to: "static",
+        },
+      ],
+    }),
+  ],
+};
