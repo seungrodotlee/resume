6d19046c43d066fe6289ef6593b68207459be264
이승로 <iseungro@iseungloui-MacBookPro.local>
2022-03-30 02:02:44 +0900

    [22.03.30:02:02] 내용 작성

--- src/index.js
+++ src/index.js
@@ -13,4 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   typeHeaderLabel();
+
+  const getWorkYear = () => {
+    const workYearElement = document.querySelector(".work-year");
+    const startYear = 2021;
+    const startMonth = 6;
+
+    const today = new Date();
+    const todayYear = today.getFullYear();
+    const todayMonth = today.getMonth() + 1;
+    const diff = (todayYear - startYear) * 12 - (startMonth - todayMonth);
+    console.log(diff);
+    const year = parseInt(diff / 12);
+    const month = parseInt(diff % 12);
+
+    const label = `${year > 0 ? `${year}년 ` : ""}${
+      month > 0 ? `${month}개월` : ""
+    } 차`;
+    workYearElement.textContent = label;
+  };
+
+  getWorkYear();
 });
